a8b44f95c077dac59aab144d95e67f3ccbe32cf8
neokin <mazkirand@gmail.com>
2016-05-30 22:52:09 +0300

    Create User.java

---
+++ lab11/src/main/java/entity/User.java
@@ -0,0 +1,23 @@
+package entity;
+
+public class User {
+
+    public String name;
+    public String pass;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
